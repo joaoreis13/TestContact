4cd6fbb43e9ce419a11790545c07bfe94c615469
João Paulo Souza Reis <joaoreis13@gmail.com>
2016-10-15 13:31:08 -0300

    0.1.1
    Removing unecessary files

--- Endpoint/msg.py
+++
@@ -1,9 +0,0 @@
-import json
-from flask import jsonify
-
-def handle(content):
-
-    tmpResponse = jsonify({'id':content['id'], 'status':'recieved'})
-    tmpResponse.status_code = 202
-
-    return tmpResponse
